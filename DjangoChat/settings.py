2a625bfa3d53e702f97d1ff178852e97d76ea01b
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2021-08-21 16:58:46 +0100

    done

--- DjangoChat/settings.py
+++ DjangoChat/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yhhn2@$sl^o@8y9p%5mgykc)^%fl$*l4eews37g)b2pv@mbt9w'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
